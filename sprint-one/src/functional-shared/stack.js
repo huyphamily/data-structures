590ae841ff943534c787b1285681ba4db0e852a1
Huy Pham <huyphamily@gmail.com>
2014-07-23 16:58:42 -0700

    finish prototypal

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,33 @@
 var makeStack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  
+  var instance = {};
+  instance.storage = {};
+  instance.length = 0;
+
+  _.extend(instance, stackMethods);
+
+
+  return instance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function (value) {
+    this.storage[this.length] = value;
+    this.length++;
+  },
+  pop: function () {
+    if (this.length > 0) {
+      this.length--;
+    }
+    var temp = this.storage[this.length];
+    delete this.storage[this.length];
+    return temp;
+  },
+  size: function () {
+    return this.length;
+  }
+};
 
 
